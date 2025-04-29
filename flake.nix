f745cd85889aa99fdf96a602109a32a1d2897f30
Kira Illarionov <us4tiyny4n@gmail.com>
2025-04-30 00:21:36 +0200

    flake: configurations from hosts, more extensible[C

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     user = "us4tiyny4n";
-    hostName = "new-moon";
+
     myConfiguration = ./configuration;
     myModules = {
         ide = {
@@ -37,24 +37,27 @@
           hyprland = import ./modules/de/hyprland.nix;
         };
     };
+
+    hosts = [
+      { hostName = "new-moon"; description = "laptop"; }
+      # { hostName = "waxing-crescent"; description = "raspberry-pi"; }
+    ];
+
+    makeSystem = { hostName }: nixpkgs.lib.nixosSystem {
+      inherit system; # same as system = system
+      specialArgs = {
+        inherit inputs user hostName myConfiguration myModules;
+	  };
+      modules = [ ./hosts/${hostName}/configuration.nix ];
+    };
   in
   {
-    nixosConfigurations = {
-      ${hostName} = nixpkgs.lib.nixosSystem {
-        inherit system; # same as system = system
-        specialArgs = {
-          inherit inputs user hostName myConfiguration myModules;
-	    };
-        modules = [
-          ./hosts/${hostName}/configuration.nix
-        ];
-      };
-      # raspberrypi = nixpkgs.lib.nixosSystem {
-      #   specialArgs = { inherit inputs; };
-      #   modules = [
-      #     ./hosts/raspberrypi/configuration.nix
-      #   ];
-      # };
-    };
+    nixosConfigurations = nixpkgs.lib.foldl' 
+      (accConfigs: aHost:
+      accConfigs // {
+        "${aHost.hostName}" = makeSystem {
+          hostName = aHost.hostName;
+        };
+      }) {} hosts;
   };
 }
