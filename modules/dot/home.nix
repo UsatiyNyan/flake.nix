1e3412e6514eb9809b06282086b5cd0cbc092ec2
UsatiyNyan <us4tiyny4n@gmail.com>
2025-05-07 21:51:40 +0200

    Home: bluetuith and fzf

--- modules/dot/home.nix
+++ modules/dot/home.nix
@@ -37,11 +37,12 @@ in {
       zoxide
       btop
       ripgrep
+      fzf
 
-      # It is sometimes useful to fine-tune packages, for example, by applying
-      # overrides. You can do that directly here, just don't forget the
-      # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # fonts?
+      # TUI
+      bluetuith
+
+      # STYLE
       (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
 
       # # You can also create simple shell scripts directly inside your
