11afedd9cf5b83d3ce5b824cb61f515ccb398f27
UsatiyNyan <us4tiyny4n@gmail.com>
2025-05-10 19:29:39 +0200

    sddm: rose-pine theme

--- optional-configuration/sddm.nix
+++ optional-configuration/sddm.nix
@@ -1,3 +1,26 @@
 {
-  services.displayManager.sddm.enable = true;
+  inputs,
+  pkgs,
+  ...
+}: let
+  rosePineTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "sddm-rose-pine-theme";
+    version = "1.2";
+    dontBuild = true;
+
+    propagatedUserEnvPkgs = [pkgs.libsForQt5.qt5.qtgraphicaleffects];
+
+    src = inputs.sddm-rose-pine;
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -aR $src $out/share/sddm/themes/rose-pine
+    '';
+  };
+in {
+  environment.systemPackages = [rosePineTheme];
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "rose-pine";
+  };
 }
