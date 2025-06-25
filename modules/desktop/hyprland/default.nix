8633010b4b960c9a4aed19e4dcae06480c9dd03f
UsatiyNyan <us4tiyny4n@gmail.com>
2025-06-25 20:35:09 +0200

    Hyprland: fix workspaces

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -50,12 +50,6 @@
         "3, monitor:$monitor0, persistent:true, on-created-empty:$notesApp"
         "4, monitor:$monitor0, persistent:true, on-created-empty:$fileManager"
         "5, monitor:$monitor0, persistent:true, on-created-empty:$messagingApp"
-
-        "6, monitor:$monitor1"
-        "7, monitor:$monitor1"
-        "8, monitor:$monitor1"
-        "9, monitor:$monitor1"
-        "10, monitor:$monitor1"
       ];
 
       windowrule = [
