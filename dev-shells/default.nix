390497110c4a465b1da02772addbd36f13265d37
Kira Illarionov <us4tiyny4n@gmail.com>
2025-10-13 00:19:03 +0200

    rework dev-shells

--- dev-shells/default.nix
+++ dev-shells/default.nix
@@ -1,11 +1,21 @@
-{nixpkgs, ...} @ inputs: let
-  waylandCpp = import ./wayland_cpp.nix;
-in {
-  "x86_64-linux" = let
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in {
-    "wayland_cpp" = pkgs.mkShell (waylandCpp {inherit pkgs;});
+{nixpkgs, ...}: let
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+
+  modules = {
+    "wayland_cpp" = import ./wayland_cpp.nix;
   };
-  "aarch64-linux" = {
-  };
-}
+
+  _modules = system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+    nixpkgs.lib.mapAttrs (name: module: pkgs.mkShell (module {inherit pkgs;}))
+    modules;
+in
+  builtins.listToAttrs (map (system: {
+      name = system;
+      value = _modules system;
+    })
+    systems)
