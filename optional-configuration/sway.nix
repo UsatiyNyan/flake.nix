e00a2b0c2c3d94fa30c98b1ebada0ca595bee7c4
UsatiyNyan <us4tiyny4n@gmail.com>
2025-11-14 23:32:01 +0100

    sway: fix dconf

--- optional-configuration/sway.nix
+++ optional-configuration/sway.nix
@@ -2,6 +2,8 @@
   pkgs,
   ...
 }: {
+  programs.dconf.enable = true;
+
   environment = {
     # TODO: systemPackages = with pkgs; [polkit_gnome];
     systemPackages = with pkgs; [hyprpolkitagent];
