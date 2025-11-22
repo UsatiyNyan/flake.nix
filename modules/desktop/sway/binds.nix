ef2ee4f9da3642e6a94bc89518818938bbd22590
UsatiyNyan <us4tiyny4n@gmail.com>
2025-11-22 21:22:37 +0100

    sway: movetoworkspace fix

--- modules/desktop/sway/binds.nix
+++ modules/desktop/sway/binds.nix
@@ -51,6 +51,12 @@
         screenshotArea = ''grim -g $(slurp)'';
         screenshotWindow = "my-grim-window";
         screenshotOutput = ''grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')'';
+        focusToWorkspace = x: "workspace number ${x}";
+        containerToWorkspace = x: "move container to workspace number ${x}";
+        focusAndContainerToWorkspace = x: let
+          ctw = containerToWorkspace x;
+          ftw = focusToWorkspace x;
+        in "${ctw}; ${ftw}";
       in {
         "$mod+Shift+t" = "exec $terminal";
         "$mod+t" = "exec $terminalTmux";
@@ -76,27 +82,27 @@
         "$mod+k" = "focus up";
         "$mod+l" = "focus right";
 
-        "$mod+1" = "workspace number 1";
-        "$mod+2" = "workspace number 2";
-        "$mod+3" = "workspace number 3";
-        "$mod+4" = "workspace number 4";
-        "$mod+5" = "workspace number 5";
-        "$mod+6" = "workspace number 6";
-        "$mod+7" = "workspace number 7";
-        "$mod+8" = "workspace number 8";
-        "$mod+9" = "workspace number 9";
-        "$mod+0" = "workspace number 10";
+        "$mod+1" = focusToWorkspace "1";
+        "$mod+2" = focusToWorkspace "2";
+        "$mod+3" = focusToWorkspace "3";
+        "$mod+4" = focusToWorkspace "4";
+        "$mod+5" = focusToWorkspace "5";
+        "$mod+6" = focusToWorkspace "6";
+        "$mod+7" = focusToWorkspace "7";
+        "$mod+8" = focusToWorkspace "8";
+        "$mod+9" = focusToWorkspace "9";
+        "$mod+0" = focusToWorkspace "10";
 
-        "$mod+Shift+1" = "move container to workspace number 1";
-        "$mod+Shift+2" = "move container to workspace number 2";
-        "$mod+Shift+3" = "move container to workspace number 3";
-        "$mod+Shift+4" = "move container to workspace number 4";
-        "$mod+Shift+5" = "move container to workspace number 5";
-        "$mod+Shift+6" = "move container to workspace number 6";
-        "$mod+Shift+7" = "move container to workspace number 7";
-        "$mod+Shift+8" = "move container to workspace number 8";
-        "$mod+Shift+9" = "move container to workspace number 9";
-        "$mod+Shift+0" = "move container to workspace number 10";
+        "$mod+Shift+1" = focusAndContainerToWorkspace "1";
+        "$mod+Shift+2" = focusAndContainerToWorkspace "2";
+        "$mod+Shift+3" = focusAndContainerToWorkspace "3";
+        "$mod+Shift+4" = focusAndContainerToWorkspace "4";
+        "$mod+Shift+5" = focusAndContainerToWorkspace "5";
+        "$mod+Shift+6" = focusAndContainerToWorkspace "6";
+        "$mod+Shift+7" = focusAndContainerToWorkspace "7";
+        "$mod+Shift+8" = focusAndContainerToWorkspace "8";
+        "$mod+Shift+9" = focusAndContainerToWorkspace "9";
+        "$mod+Shift+0" = focusAndContainerToWorkspace "10";
 
         "$mod+Ctrl+h" = "workspace prev_on_output";
         "$mod+Ctrl+l" = "workspace next_on_output";
