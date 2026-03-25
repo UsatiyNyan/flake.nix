1f72ed0e3abd0be79d6772ba5e94e6b2203e2bb4
UsatiyNyan <us4tiyny4n@gmail.com>
2026-03-25 19:05:55 +0100

    claude

--- modules/ide/ai.nix
+++ modules/ide/ai.nix
@@ -1,6 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}: let
+  pkgs-unstable =
+    import inputs.nixpkgs-unstable
+    {
+      inherit system;
+      config.allowUnfree = true;
+    };
+in {
   home = {
-    packages = with pkgs; [aider-chat claude-code];
+    packages = (with pkgs; [aider-chat]) ++ (with pkgs-unstable; [claude-code]);
     file = {
       ".local/.gitignore".text = ''
         .aider*
