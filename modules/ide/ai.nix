6c0780e7f5b89092bd99aa620697f350936ce8b3
UsatiyNyan <us4tiyny4n@gmail.com>
2026-04-23 00:05:01 +0200

    my-claude setup with nodejs

--- modules/ide/ai.nix
+++ modules/ide/ai.nix
@@ -10,9 +10,16 @@
       inherit system;
       config.allowUnfree = true;
     };
+  my-claude = pkgs.writeShellApplication {
+    name = "my-claude";
+    runtimeInputs = [pkgs.nodejs pkgs-unstable.claude-code];
+    text = ''
+      claude "$@"
+    '';
+  };
 in {
   home = {
-    packages = (with pkgs; [aider-chat]) ++ (with pkgs-unstable; [claude-code]);
+    packages = (with pkgs; [aider-chat]) ++ (with pkgs-unstable; [claude-code]) ++ [my-claude];
     file = {
       ".local/.gitignore".text = ''
         .aider*
