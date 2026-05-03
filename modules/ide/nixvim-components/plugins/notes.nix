f423930e4ddb4903537ff9cd1852f3c97aab8a0b
UsatiyNyan <us4tiyny4n@gmail.com>
2026-05-03 22:10:04 +0200

    vimwiki: markdown

--- modules/ide/nixvim-components/plugins/notes.nix
+++ modules/ide/nixvim-components/plugins/notes.nix
@@ -1,6 +1,17 @@
 {...}: {
   plugins = {
-    vimwiki.enable = true;
+    vimwiki = {
+      enable = true;
+      settings = {
+        list = [
+          {
+            ext = ".md";
+            path = "~/Documents/notes/";
+            syntax = "markdown";
+          }
+        ];
+      };
+    };
     cmp-vimwiki-tags.enable = true;
   };
 }
