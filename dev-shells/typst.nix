2f7a63a1aa8bf511bdbfd652c6a29b791a28345f
UsatiyNyan <us4tiyny4n@gmail.com>
2026-05-14 22:51:59 +0200

    typst preview

--- dev-shells/typst.nix
+++ dev-shells/typst.nix
@@ -8,6 +8,17 @@
         enable = true;
         settings = {
           formatterMode = "typstfmt";
+          outputPath = "$root/$dir/$name";
+        };
+      };
+      typst-preview = {
+        enable = true;
+        settings = {
+          port = 7457;
+          dependencies_bin = {
+            tinymist = null;
+          };
+          invert_colors = "auto";
         };
       };
       treesitter.settings.ensure_installed = ["typst"];
