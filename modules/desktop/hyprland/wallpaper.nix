107983d7a5e3ce6bc7af362cf436ec4bd42bc315
Kira Illarionov <us4tiyny4n@gmail.com>
2026-05-17 12:51:59 +0200

    hyprpaper configuration

--- modules/desktop/hyprland/wallpaper.nix
+++ modules/desktop/hyprland/wallpaper.nix
@@ -1,11 +1,34 @@
-{pkgs, ...}: {
-  home = {
-    packages = with pkgs; [hyprpaper];
-    file.".config/hypr/hyprpaper.conf".text = let
-      wallpaperPath = "$HOME/Pictures/wallpaper.jpg";
-    in ''
-      preload = ${wallpaperPath}
-      wallpaper = , ${wallpaperPath}
-    '';
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.hyprpaper;
+in {
+  options.hyprpaper = {
+    myDefaultWallpaper = lib.mkOption {
+      type = lib.types.str;
+      default = "$HOME/Pictures/wallpaper.jpg";
+      description = "Default wallpaper path";
+    };
+    myExtraPreload = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Additional wallpapers to preload";
+    };
+  };
+
+  config = {
+    home = {
+      packages = with pkgs; [hyprpaper];
+      file.".config/hypr/hyprpaper.conf".text = let
+        allPreloads = [cfg.myDefaultWallpaper] ++ cfg.myExtraPreload;
+        preloadLines = lib.concatMapStringsSep "\n" (p: "preload = ${p}") allPreloads;
+      in ''
+        ${preloadLines}
+        wallpaper = , ${cfg.myDefaultWallpaper}
+      '';
+    };
   };
 }
